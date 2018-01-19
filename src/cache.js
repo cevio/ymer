16f8d6d70121b9e5de56076d484716d66af9e083
shenyunjie <shenyunjie@u51.com>
2018-01-19 22:06:01 +0800

    update

--- src/cache.js
+++ src/cache.js
@@ -47,6 +47,7 @@ module.exports = class Cache {
             // 直接Stringify后存入
             if (Array.isArray(result)) {
               if (result.length) {
+                await redis.del(name);
                 await redis.hmset(name, {
                   __ArrayStringify__: true,
                   value: JSON.stringify(result)
@@ -66,6 +67,7 @@ module.exports = class Cache {
           }
           // 其余任何类型直接转化为字符串存入
           else {
+            await redis.del(name);
             await redis.hmset(name, {
               __Stringify__: util.type(result),
               value: result
@@ -83,7 +85,6 @@ module.exports = class Cache {
     const name = this.urlPen.select(domain, ...pathname).toString(args);
     const redis = await this.yme.redis();
     const exists = await this.compress(await redis.exists(name));
-
     if (exists) {
       const values = await this.compress(await redis.hgetall(name));
       if (values.__Stringify__) {
@@ -103,9 +104,7 @@ module.exports = class Cache {
       values = await new Promise((resolve, reject) => {
         values.exec_atomic((err, replies) => {
           if (err) return reject(err);
-          const value = replies.slice(1);
-          if (value.length > 1) return resolve(value);
-          resolve(value[0]);
+          resolve(replies[replies.length - 1]);
         });
       });
     }
