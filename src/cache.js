45543aeee711e482879ea03ed4d60847b57aa8d2
shenyunjie <shenyunjie@u51.com>
2018-01-19 21:39:51 +0800

    update

--- src/cache.js
+++ src/cache.js
@@ -47,6 +47,7 @@ module.exports = class Cache {
             // 直接Stringify后存入
             if (Array.isArray(result)) {
               if (result.length) {
+                await redis.del(name);
                 await redis.hmset(name, {
                   __ArrayStringify__: true,
                   value: JSON.stringify(result)
@@ -58,6 +59,7 @@ module.exports = class Cache {
             // 通过encode编码后存入
             else {
               if (Object.keys(result).length) {
+                await redis.del(name);
                 await redis.hmset(name, util.encode(result));
                 await this.expire(name, ret.time);
               }
@@ -65,6 +67,7 @@ module.exports = class Cache {
           }
           // 其余任何类型直接转化为字符串存入
           else {
+            await redis.del(name);
             await redis.hmset(name, {
               __Stringify__: util.type(result),
               value: result
