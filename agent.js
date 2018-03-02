b6b276a9431be60f1dc116a1a9fa822c8a8f70a1
shenyunjie <shenyunjie@u51.com>
2018-03-02 13:48:39 +0800

    update

--- agent.js
+++ agent.js
@@ -31,24 +31,28 @@ module.exports = async function(agent) {
 
   const timer = setInterval(task, 1 * 60 * 1000);
   
-  app.health.add(async () => {
-    return {
-      key: 'mysql',
-      value: mysqlStatus 
-        ? { status: 'UP', count: mysqlValue, last_check_time: mysqlLastCheckTime } 
-        : { status: 'OUT_OF_SERVICE', count: mysqlValue, last_check_time: mysqlLastCheckTime }
-    }
-  });
+  if (config.widgets.mysql) {
+    app.health.add(async () => {
+      return {
+        key: 'mysql',
+        value: mysqlStatus 
+          ? { status: 'UP', count: mysqlValue, last_check_time: mysqlLastCheckTime } 
+          : { status: 'OUT_OF_SERVICE', count: mysqlValue, last_check_time: mysqlLastCheckTime }
+      }
+    });
+  }
 
-  app.health.add(async () => {
-    return {
-      key: 'redis',
-      value: redisStatus 
-        ? { status: 'UP', last_check_time: redisLastCheckTime } 
-        : { status: 'OUT_OF_SERVICE', last_check_time: redisLastCheckTime }
-    }
-  });
-
+  if (config.widgets.redis) {
+    app.health.add(async () => {
+      return {
+        key: 'redis',
+        value: redisStatus 
+          ? { status: 'UP', last_check_time: redisLastCheckTime } 
+          : { status: 'OUT_OF_SERVICE', last_check_time: redisLastCheckTime }
+      }
+    });
+  }
+  
   app.on('mounted', task);
   app.on('beforeDestroy', async () => {
     clearInterval(timer);
@@ -59,25 +63,29 @@ module.exports = async function(agent) {
     await ymer.exec(async (yme) => {
       const time = new Date();
 
-      try {
-        const mysql = await yme.mysql();
-        const res = await mysql.exec(`select COUNT(table_name) AS Count from information_schema.tables where table_schema='${config.widgets.mysql.database}'`);
-        mysqlValue = res[0].Count;
-        mysqlStatus = true;
-      } catch(e) { 
-        mysqlStatus = false;
-        mysqlValue = 0;
+      if (config.widgets.mysql) {
+        try {
+          const mysql = await yme.mysql();
+          const res = await mysql.exec(`select COUNT(table_name) AS Count from information_schema.tables where table_schema='${config.widgets.mysql.database}'`);
+          mysqlValue = res[0].Count;
+          mysqlStatus = true;
+        } catch(e) { 
+          mysqlStatus = false;
+          mysqlValue = 0;
+        }
+        mysqlLastCheckTime = time;
       }
       
-      try {
-        const redis = await yme.redis();
-        await redis.set(`${config.widgets.redis.name}:sys:alive`, time.getTime() + '');
-        redisStatus = true;
-      } catch(e) {
-        redisStatus = false;
+      if (config.widgets.redis) {
+        try {
+          const redis = await yme.redis();
+          await redis.set(`${config.widgets.redis.name}:sys:alive`, time.getTime() + '');
+          redisStatus = true;
+        } catch(e) {
+          redisStatus = false;
+        }
+        redisLastCheckTime = time;
       }
-
-      redisLastCheckTime = mysqlLastCheckTime = time;
     });
   }
 }
